d8d607b96f991021942e55e10f8641a53a926a83
json-jh <dev.json.jh@gmail.com>
2024-04-21 23:05:24 +0900

    init : router

--- src/router/routes.tsx
+++ src/router/routes.tsx
@@ -1,6 +1,9 @@
 import { RouteObject, createBrowserRouter } from "react-router-dom";
+
+import Auth from "./middleware/Auth";
+
 import Layout from "../layouts/default";
-import Auth from "../middleware/Auth";
+
 import About from "../pages/About";
 import Home from "../pages/Home";
 import Login from "../pages/Login";
@@ -10,21 +13,18 @@ type customRouteObject = RouteObject & {
   children?: customRouteObject[];
 };
 
-const routeObjects: customRouteObject[] = [
+export const routes: customRouteObject[] = [
   {
-    path: "/",
     element: <Layout />,
     auth: true,
     children: [
       {
         path: "/",
         element: <Home />,
-        auth: true,
       },
       {
         path: "/about",
         element: <About />,
-        auth: true,
       },
     ],
   },
@@ -34,18 +34,19 @@ const routeObjects: customRouteObject[] = [
   },
 ];
 
-const protecting = (routes: customRouteObject[], parentAuth?: boolean) => {
+const protecting = (routes: customRouteObject[], auth?: boolean) => {
   routes.forEach((route) => {
-    if (route.auth === true || parentAuth === true) {
+    if (route.auth === true || auth === true) {
       route.element = <Auth element={route.element} />;
+      route.auth = true;
     }
     if (Array.isArray(route.children)) {
-      protecting(route.children, route.auth);
+      protecting(route.children, auth || route.auth);
     }
   });
   return routes;
 };
 
-const router = createBrowserRouter(protecting(routeObjects));
+const router = createBrowserRouter(protecting(routes));
 
 export default router;
